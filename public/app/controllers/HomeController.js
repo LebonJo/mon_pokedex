6be45beaa8f633acad9cfeb54986f2a31a27cf64
Jonathan Lebon <jonathan.lebon@outlook.com>
2015-07-31 09:21:56 +0200

    Pagination

--- public/app/controllers/HomeController.js
+++ public/app/controllers/HomeController.js
@@ -9,6 +9,7 @@ angular.module('pokedex')
 	homeCtrl.currentPage = 1;
 	homeCtrl.totalItems = 721;
 	homeCtrl.itemsPerPage = 30;
+	homeCtrl.maxSize = 5;
 
 	function fetchPokedex (){
 		var bottomLimit = (homeCtrl.currentPage-1) * 30;
